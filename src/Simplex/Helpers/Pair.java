5afb88cfe1f05cf90817cf61314f68170bc1be5f
MAzhar <hossain.mazharul@gmail.com>
2016-11-29 12:33:45 +0600

    Practice of Simplex Method-2

---
+++ src/Simplex/Helpers/Pair.java
@@ -0,0 +1,29 @@
+package Simplex.Helpers;
+
+public class Pair<T1, T2> {
+    public T1 firstEle;
+    public T2 secondEle;
+
+    public Pair(T1 firstEle, T2 secondEle) {
+        if (firstEle == null || secondEle == null)
+            throw new NullPointerException();
+        this.firstEle = firstEle;
+        this.secondEle = secondEle;
+    }
+
+    public T1 getFirstEle() {
+        return this.firstEle;
+    }
+
+    public void setFirstEle(T1 value) {
+        this.firstEle = value;
+    }
+
+    public T2 getSecondEle() {
+        return this.secondEle;
+    }
+
+    public void setSecondEle(T2 value) {
+        this.secondEle = value;
+    }
+}
---
+++ src/Simplex/MyFrame.java
@@ -0,0 +1,104 @@
+package Simplex;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+
+public class MyFrame extends JFrame {
+    /**
+     *
+     */
+    private static final long serialVersionUID = -6417002867509503344L;
+    private ImageIcon image = new ImageIcon("image\\0018.jpg");
+    private JLabel objFuncLabel = new JLabel("Ŀ�꺯����");
+    private JTextField objFunction = new JTextField();
+    private JLabel constraintsLabel = new JLabel("Լ��������");
+    private JTextArea constraintsArea = new JTextArea();
+    private JButton button = new JButton("���");
+    private JLabel solutionLabel = new JLabel("���Ž⣺");
+    private JTextArea solutionShow = new JTextArea();
+
+    public MyFrame() {
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        setSize(screenSize.width / 2, screenSize.height / 2);
+        setIconImage(image.getImage());
+        setTitle("Simplex Method");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        initComponents();
+
+    }
+
+    public static void main(String[] args) {
+        new MyFrame();
+    }
+
+    private void initComponents() {
+        Container container = getContentPane();
+        GroupLayout layout = new GroupLayout(container);
+        container.setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+
+        GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
+        hGroup.addGroup(layout.createParallelGroup().addComponent(objFuncLabel).addComponent(objFunction).addComponent(constraintsLabel).addComponent(constraintsArea).addComponent(button));
+        hGroup.addGroup(layout.createParallelGroup().addComponent(solutionLabel).addComponent(solutionShow));
+        layout.setHorizontalGroup(hGroup);
+
+        GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
+        vGroup.addGroup(layout.createParallelGroup().addComponent(objFuncLabel));
+        vGroup.addGroup(layout.createParallelGroup().addComponent(objFunction));
+        vGroup.addGroup(layout.createParallelGroup().addComponent(constraintsLabel).addComponent(solutionLabel));
+        vGroup.addGroup(layout.createParallelGroup().addComponent(constraintsArea).addComponent(solutionShow));
+        vGroup.addGroup(layout.createParallelGroup().addComponent(button));
+        layout.setVerticalGroup(vGroup);
+
+        button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                System.out.println("Button clicked");
+                // TODO Auto-generated method stub
+                String objectiveFunc = objFunction.getText().trim();
+                System.out.println("objectiveFunc: " + objectiveFunc);
+                String constraints = constraintsArea.getText().trim();
+                System.out.println("constraints: " + constraints);
+
+                if (objectiveFunc.equals("") || constraints.equals("")) {
+                    return;
+                }
+                String[] subConstraint = constraints.split("\n");
+                ArrayList<String> constraintList = new ArrayList<String>();
+                for (int i = 0; i < subConstraint.length; i++) {
+                    constraintList.add(subConstraint[i]);
+                }
+                process(objectiveFunc, constraintList);
+            }
+        });
+    }
+
+    private void process(final String objectiveFunc, final ArrayList<String> constraints) {
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                Normalization normalization = new Normalization(objectiveFunc, constraints);
+                normalization.transferToCanonicalForm();
+//				normalization.objectiveFuncPrint();
+//				normalization.canonicalFormPrint();
+//				normalization.constantTermPrint();
+                SimplexTable st = new SimplexTable(normalization.getMaxProblemState());
+                st.initSimplexTable(normalization.getNumOfDecisionVar(), normalization.getCoeffOfMatrix(),
+                        normalization.getCoeffOfDecisionVar(), normalization.getConstantTermList());
+                st.execIteration();
+                solutionShow.setText(st.getStringBuffer().toString());
+            }
+        }).start();
+    }
+}
